51aca0f0794a3e7c0c90a3fb25c35d61c1c47caa
Ajith Kumar <ajithpoojary1798@gmail.com>
2023-02-25 16:30:17 +0530

    guessing game

---
+++ 01-Guess-My-Number/starter/script.js
@@ -0,0 +1,49 @@
+"use strict";
+
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+let score = 20;
+let highScore = 0;
+
+document.querySelector(".check").addEventListener("click", function () {
+  const guess = Number(document.querySelector(".guess").value);
+  console.log(guess);
+
+  if (!guess) {
+    document.querySelector(".message").textContent = "⛔No number";
+  }
+  if (score > 1) {
+    if (guess == secretNumber) {
+      document.querySelector(".number").textContent = secretNumber;
+      document.querySelector(".message").textContent = "🎉 Correct answer!!";
+      document.querySelector("body").style.backgroundColor = "#60b347";
+      document.querySelector(".number").style.width = "30rem";
+      if (score > highScore) {
+        highScore = score;
+        document.querySelector(".highscore").textContent = highScore;
+      }
+    } else if (guess > secretNumber) {
+      document.querySelector(".message").textContent = "📈 Too high!";
+      score--;
+      document.querySelector(".score").textContent = score;
+    } else if (guess < secretNumber) {
+      document.querySelector(".message").textContent = "📉 Too low!";
+      score--;
+      document.querySelector(".score").textContent = score;
+    }
+  } else {
+    document.querySelector(".message").textContent = "🎇You lost the game";
+    document.querySelector(".score").textContent = 0;
+  }
+});
+
+document.querySelector(".again").addEventListener("click", function () {
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  score = 20;
+  document.querySelector(".message").textContent = "Start guessing...";
+  document.querySelector(".score").textContent = score;
+  document.querySelector(".number").textContent = "?";
+  document.querySelector(".guess").value = " ";
+  document.querySelector(".number").style.width = "15rem";
+  document.querySelector("body").style.backgroundColor = "#222";
+});
